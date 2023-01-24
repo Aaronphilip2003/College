52b5ea9bbab03f7cb3c1d7b4a9a650485495eb0e
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-01-24 10:26:30 +0530

    Initial commit

--- MAIOT/Programs/ASCII/ascii.asm
+++ MAIOT/Programs/ASCII/ascii.asm
@@ -1,11 +1,15 @@
-; if you give an input 45 it will be entered as [34,35,"ascii value of enter key"],
-; ASCII value of 4 is 34
-; ASCII value of 5 is 35
-; if you press enter the ASCII value of the enter key will also get saved
-; we need to convert 34 and 35 to 4 and 5
-; rotate operation is performed bit by bit
-; rotate 04 we will get 40 then add 5
-; steps first subtract 30 or 37, then rotate the higher digit and add the lower digit
-; 34 35 --> 04, 05 -->rotate higher bit ---> 40,05---> add them ----> therefore ans is 45
+mov al,0
+mov rdi,num
+mov cl,02
 
+up1:mov b1,byte[num]
+cmp bl,39h
+jbe down1
+sub b1,07h
 
+down1:sub b1,30h
+rol al,4
+add al,bl
+inc rdi
+dec cx
+jnz up1
