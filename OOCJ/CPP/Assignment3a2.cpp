4b652c57f29cd2de1bcd253dd10ece4d0558c8d8
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-02-20 09:02:53 +0530

    Initial commit

--- OOCJ/CPP/Assignment3a2.cpp
+++ OOCJ/CPP/Assignment3a2.cpp
@@ -14,6 +14,8 @@ public:
     Complex(float x, float y);
     void display();
     friend Complex operator+(Complex &a, Complex &b);
+    friend Complex operator-(Complex &a, Complex &b);
+    friend Complex operator*(Complex &a, Complex &b);
 };
 
 Complex::Complex(float x, float y)
@@ -30,6 +32,22 @@ Complex operator+(Complex &ca, Complex &cb)
     return temp;
 }
 
+Complex operator-(Complex &ca, Complex &cb)
+{
+    Complex temp;
+    temp.real = ca.real - cb.real;
+    temp.imag = ca.imag - cb.imag;
+    return temp;
+}
+
+Complex operator*(Complex &ca, Complex &cb)
+{
+    Complex temp;
+    temp.real = ca.real * cb.real;
+    temp.imag = ca.imag * cb.imag;
+    return temp;
+}
+
 void Complex::display()
 {
     cout << real << " + " << imag << "i" << endl;
@@ -37,10 +55,14 @@ void Complex::display()
 
 int main()
 {
-    Complex c1, c2, c3;
+    Complex c1, c2, c3, c4, c5;
     c1 = Complex(2.5, 3.5);
     c2 = Complex(2.4, 3.6);
     c3 = c1 + c2;
+    c4 = c1 - c2;
+    c5 = c1 * c2;
     c3.display();
+    c4.display();
+    c5.display();
     return 0;
 }
